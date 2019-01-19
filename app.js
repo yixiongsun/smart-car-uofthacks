73f6f5e36f41f411cfa87f5cedce93e2ba4fb065
Yixiong Sun <yixiongsun@Maxs-MacBook-Pro.local>
2019-01-19 13:05:42 -0500

    port env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const auth = require('./routes/auth')
 const vehicle = require('./routes/vehicles')
 app.use('/login', auth)
